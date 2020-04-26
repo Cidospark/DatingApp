b5c917422bfde1b77912291b1267ce1842a2f07e
cidospark <cidospark27@gmail.com>
2020-04-26 08:05:15 +0100

    Added component communication between parent and child and back

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
+
   constructor() { }
 
   ngOnInit() {
@@ -17,7 +20,7 @@ export class RegisterComponent implements OnInit {
   }
 
   cancel() {
-    console.log ('cancelled');
+    this.cancelRegister.emit(false);
   }
 
 }
